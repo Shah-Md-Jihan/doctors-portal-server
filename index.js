cf698b6f1bff396d10a4b52c69562e49f6eacaf5
Shah Md. Jihan <shahmohammadjihan@gmail.com>
2022-11-18 23:01:49 +0600

    emailwise booking get api added

--- index.js
+++ index.js
@@ -34,6 +34,15 @@ async function run() {
       res.send(options);
     });
 
+    /*------------------booking insert api--------------------*/
+
+    // emailwise booking api
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    });
     // booking insert api
     app.post("/booking", async (req, res) => {
       const booking = req.body;
